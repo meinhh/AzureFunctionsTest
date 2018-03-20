d5b63577994334e79c3c0e44d416cb892bcbe0b2
Meirav Shenhar <meinhh@gmail.com>
2018-03-20 21:35:25 +0200

    with params

--- Hello/index.js
+++ Hello/index.js
@@ -1,21 +1,17 @@
 module.exports = (context, req) => {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    context.res = {
-        body: 'Hail Hydra'
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hail Hydra " + (req.query.name || req.body.name)
+        };
     }
-
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.res = {
-    //         // status: 200, /* Defaults to 200 */
-    //         body: "Hello " + (req.query.name || req.body.name)
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         status: 400,
-    //         body: "Please pass a name on the query string or in the request body"
-    //     };
-    // }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
     context.done();
 };
